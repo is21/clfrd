bfff22168d6ad9f20974c1ad472e8a196167a90e
is21 <ischiffer@gmail.com>
2023-11-03 14:56:32 -0700

    summaries can now handle long form articles

--- popup.js
+++ popup.js
@@ -1,71 +1,105 @@
-document.addEventListener('DOMContentLoaded', function () {
-  chrome.storage.sync.get('openai_api_key', function (data) {
+document.addEventListener('DOMContentLoaded', function() {
+  chrome.storage.sync.get('openai_api_key', function(data) {
     var openai_api_key = data.openai_api_key;
 
-    // If the API key is not set, display the input field and save button
     if (!openai_api_key) {
-      document.getElementById('apiKeyInput').style.display = 'block';
-      document.getElementById('save').addEventListener('click', function () {
-        var openai_api_key = document.getElementById('openai_api_key').value;
-        chrome.storage.sync.set({ openai_api_key: openai_api_key }, function () {
-          // Reload the popup to start the summary generation process
-          window.location.reload();
+        document.getElementById('apiKeyInput').style.display = 'block';
+        document.getElementById('save').addEventListener('click', function() {
+          var openai_api_key = document.getElementById('openai_api_key').value;
+          chrome.storage.sync.set({openai_api_key: openai_api_key}, function() {
+            window.location.reload();
+          });
         });
-      });
-      return;
+        return;
     }
 
-    // Hide the input field and save button
     document.getElementById('apiKeyInput').style.display = 'none';
-
-    // Display the loading message only if the API key is present
     document.getElementById('loadingMessage').textContent = "Generating summary...";
 
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
+        target: {tabId: tabs[0].id},
         files: ['readability.js']
-      }, function () {
-        chrome.runtime.sendMessage({ message: "get_readability_result" }, function (response) {
-          // Check if response.result is not null
+      }, function() {
+        chrome.runtime.sendMessage({message: "get_readability_result"}, function(response) {
           if (response.result) {
-            // Convert the readability result to a string
-            let resultString = JSON.stringify(response.result);
-
-            // Truncate the result string to 4000 characters
-            let truncatedResult = resultString.substring(0, 4000);
-
-            // Send the truncated readability object to the GPT API
-            fetch('https://api.openai.com/v1/engines/text-davinci-002/completions', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + openai_api_key
-              },
-              body: JSON.stringify({
-                prompt: `
-                Please generate a neutral and concise summary of the following article, focusing on the central arguments, findings, and conclusions. Exclude any supporting examples or details unless they are essential to understanding the main points. 
-                Begin your summary with "Summary:" to clearly indicate the start of the summarized content.
-                ${truncatedResult}
-            `,
-                max_tokens: 500
-              }),
-            })
+            let content = response.result.textContent;
+            let contentLength = content.length;
+            let maxRequestSize = 4096; // Adjust this to the maximum request size for the API
+            let numChunks = Math.ceil(contentLength / maxRequestSize);
+            let chunkSize = Math.ceil(contentLength / numChunks);
+            let contentParts = content.match(new RegExp('.{1,' + chunkSize + '}', 'g'));
+            let summaries = [];
+            let promises = contentParts.map(part => {
+              return fetch('https://api.openai.com/v1/chat/completions', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': 'Bearer ' + openai_api_key
+                },
+                body: JSON.stringify({
+                  model: "gpt-3.5-turbo",
+                  temperature: 0.5,
+                  messages: [{
+                    role: "system",
+                    content: "You are a helpful assistant."
+                  }, {
+                    role: "user",
+                    content: `Summarize the following text in a single paragraph: ${part}`
+                  }]
+                }),
+              })
               .then(response => response.json())
               .then(data => {
-                // Hide the loading message
-                document.getElementById('loadingMessage').style.display = 'none';
-
-                // Display the API response in the popup
-                document.getElementById('readability').innerHTML = data.choices[0].text.trim();
-              })
-              .catch(error => {
-                console.log(error);
-                // Display an error message
-                document.getElementById('readability').textContent = "Could not get a response from the GPT API.";
+                console.log(data);
+                // Check if the choices array exists and has at least one element
+                if (data.choices && data.choices.length > 0 && data.choices[0].message && data.choices[0].message.content) {
+                  let summary = data.choices[0].message.content.trim();
+                  summaries.push(summary);
+                } else {
+                  console.log("Unexpected API response:", data);
+                }
               });
+            });
+
+            Promise.all(promises)
+            .then(() => {
+              let combinedSummary = summaries.join(' ');
+              return fetch('https://api.openai.com/v1/chat/completions', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': 'Bearer ' + openai_api_key
+                },
+                body: JSON.stringify({
+                  model: "gpt-3.5-turbo",
+                  temperature: 0.5,
+                  messages: [{
+                    role: "system",
+                    content: "You are a helpful assistant."
+                  }, {
+                    role: "user",
+                    content: `Summarize the following text prefaced with "Text:"
+                    The summary should contain a summarized title wrapped in an H1 tag.
+                    The body should be broken into easily digestible paragraphs wrapped in P tags.
+                    The body of the summary should be no more than 3 paragraphs
+                     Text: ${combinedSummary}`
+                  }]
+                }),
+              });
+            })
+            .then(response => response.json())
+            .then(data => {
+              // Check if the choices array exists and has at least one element
+              if (data.choices && data.choices.length > 0 && data.choices[0].message && data.choices[0].message.content) {
+                let finalSummary = data.choices[0].message.content.trim();
+                document.getElementById('loadingMessage').style.display = 'none';
+                document.getElementById('readability').innerHTML = finalSummary;
+              } else {
+                console.log("Unexpected API response:", data);
+              }
+            })
           } else {
-            // Display an error message
             document.getElementById('readability').textContent = "Could not parse the page.";
           }
         });
